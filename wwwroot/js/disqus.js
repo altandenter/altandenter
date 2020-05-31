e257491a5712045d9ce4915f0d6a30d13343233b
bartvanhoey <bartvanhoey@hotmail.com>
2020-05-31 19:01:08 +0200

    update

--- wwwroot/js/disqus.js
+++ wwwroot/js/disqus.js
@@ -16,21 +16,3 @@
         }
     };
 })();
-
-
-// // /* <div id="disqus_thread"></div>
-//     <script>
-
-//         var disqus_config = function () {
-//             this.page.url = "https://altandenter.github.io/blogpost001";
-//     this.page.identifier = blog001;
-// };
-
-// (function() {
-//             let d = document, s = d.createElement('script');
-// s.src = 'https://https-altandenter-github-io.disqus.com/embed.js';
-// s.setAttribute('data-timestamp', +new Date());
-// (d.head || d.body).appendChild(s);
-// })();
-// </script>
-//     <noscript>Please enable JavaScript to view the <a href="https://disqus.com/?ref_noscript">comments powered by Disqus.</a></noscript> */}
