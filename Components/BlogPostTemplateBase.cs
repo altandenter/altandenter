41ad9320b8f29d7b4b00875087f85408b16c41ac
bartvanhoey <bartvanhoey@hotmail.com>
2020-06-01 21:12:04 +0200

    blogpost4 wip

--- Components/BlogPostTemplateBase.cs
+++ Components/BlogPostTemplateBase.cs
@@ -1,10 +1,18 @@
+using System.Threading.Tasks;
 using Microsoft.AspNetCore.Components;
+using Microsoft.JSInterop;
 
 namespace AltAndEnter.Pages.Blog.BlogPosts
 {
     public class BlogPostTemplateBase : ComponentBase
     {
+        [Inject] IJSRuntime JSRuntime { get; set; }
         [Parameter] public string Title { get; set; }
         [Parameter] public RenderFragment BlogPost { get; set; }
+
+        protected override async Task OnAfterRenderAsync(bool firstRender)
+        {
+            await JSRuntime.InvokeVoidAsync("Prism.highlightAll");
+        }
     }
 }
