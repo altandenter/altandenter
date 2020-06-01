f8084a5cb61194aa630424df484d3b530b85dcb0
bartvanhoey <bartvanhoey@hotmail.com>
2020-06-01 21:47:32 +0200

    blogpost004

--- Components/BlogPostTemplateBase.cs
+++ Components/BlogPostTemplateBase.cs
@@ -8,6 +8,8 @@ namespace AltAndEnter.Pages.Blog.BlogPosts
     {
         [Inject] IJSRuntime JSRuntime { get; set; }
         [Parameter] public string Title { get; set; }
+        [Parameter] public string UrlPart { get; set; }
+        [Parameter] public string PageIdentifier { get; set; }
         [Parameter] public RenderFragment BlogPost { get; set; }
 
         protected override async Task OnAfterRenderAsync(bool firstRender)
