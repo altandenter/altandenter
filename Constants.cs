eced4514d29a977b52df3d90ed4bbaa0b77514e8
bartvanhoey <bartvanhoey@hotmail.com>
2020-08-14 21:09:57 +0200

    update

--- Constants.cs
+++ Constants.cs
@@ -2,16 +2,47 @@ using Microsoft.AspNetCore.Components;
 
 namespace AltAndEnter
 {
-    public static class Constants
+  public static class Constants
+  {
+
+    public static class BlogPost001
     {
-        public static class Title
-        {
-            public static string BlogPost001 = "How to deploy a Blazor Webassembly App to GitHub Pages?";
-            public static string BlogPost002 = "How to automate the deployment of a Blazor Wasm App to GitHub Pages?";
-            public static string BlogPost003 = "How to show a location on a Leaflet map in a Blazor Wasm App?";
-            public static string BlogPost004 = "How to add Disqus to your Blazor Webassembly App?";
-        }
+      public const string Title = "How to deploy a Blazor Webassembly App to GitHub Pages?";
+      public const string Href = "blogpost001";
+      public const string PageIdentifier = "001";
+    }
+
+    public static class BlogPost002
+    {
+      public const string Title = "How to automate the deployment of a Blazor Wasm App to GitHub Pages?";
+      public const string Href = "blogpost002";
+      public const string PageIdentifier = "002";
+    }
+
+    public static class BlogPost003
+    {
+      public const string Title = "How to show a location on a Leaflet map in a Blazor Wasm App?";
+      public const string Href = "blogpost003";
+      public const string PageIdentifier = "003";
+    }
+
+    public static class BlogPost004
+    {
+      public const string Title = "How to add Disqus to your Blazor Webassembly App?";
+      public const string Href = "blogpost004";
+      public const string PageIdentifier = "004";
 
 
     }
+
+    public static class BlogPost005
+    {
+      // public static string Title = "How to add Disqus to your Blazor Webassembly App?";
+    }
+
+  }
+
+  public abstract class BlogPostBase
+  {
+  }
 }
